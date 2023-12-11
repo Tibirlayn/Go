15abb8e17a84e7a10ecd722104ac6e354ed9f65c
Tibirlayn <tibirlayn@gmail.com>
2023-12-11 21:29:17 +0300

    change go.sum

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
 
-func main()  {
-	fmt.Println("Hello word")
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// Подключение к базе данных (здесь используется PostgreSQL, но можно выбрать другую базу данных)
+	db, err := sql.Open("postgres", "user=username dbname=youdb sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	fmt.Println("Сервер запущен на http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+	
 }
